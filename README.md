c18561f5e9132ff201ffbab516ba0b9286232d78
Bright Amoateng <bright.amoateng150@gmail.com>
2020-03-06 15:46:19 -0500

    Dartboard commit

--- README.md
+++ README.md
@@ -8,18 +8,20 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ - Creates a dartboard in which darts can be thrown at
+ - Plays a game with 2 players taking turns throwing darts
+ - Uses a Monte Carlo simulation to determine an approximation of the value pi 
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 5/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ none
 
 ## REFERENCES:
- < List any outside resources used >
+ none
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ none
--- lab4.py
+++ lab4.py
@@ -32,6 +32,109 @@ import time
 #                   Your Code Goes Below                #
 #########################################################
 
+def drawSquare(myturtle, width, top_left_x, top_left_y):
+    """ Draw a square with a turtle """
+
+    myturtle.up()
+    myturtle.goto(top_left_x, top_left_y)
+    myturtle.down()
+
+    for i in range(4):
+        myturtle.forward(width)
+        myturtle.right(90)
+    
+
+def drawLine(myturtle, x_start, y_start, x_end, y_end):
+    """ Draw a line with a turtle """
+
+    myturtle.up()
+    myturtle.goto(x_start, y_start)
+    myturtle.down()
+    myturtle.goto(x_end, y_end)
+
+def drawCircle(myturtle, radius):
+    """ Draw a circle with a turtle """
+
+    myturtle.up()
+    myturtle.goto(0, -radius)
+    myturtle.down()
+    myturtle.circle(1, 360, 360)
+
+def setUpDartboard(mywindow, myturtle):
+    """ Create the dartboard in a window with a turtle """
+
+    mywindow.setworldcoordinates(-1, -1, 1, 1)
+    drawSquare(myturtle, 2, -1, 1)
+    drawLine(myturtle, 0, 1, 0, -1)
+    drawLine(myturtle, -1, 0, 1, 0)
+    drawCircle(myturtle, 1)
+
+def isInCircle(myturtle):
+    """ Determine if a turtle is in the circle radius"""
+
+    distance = myturtle.distance(0, 0)
+    return distance <= 1
+
+def throwDart(myturtle):
+    """
+    Mark a random position with a turtle, signifying 'throwing a dart'
+    The dart is lime colored when in the circle. Dark red otherwise.
+    """
+    size_of_dart = 10
+    dart_x = random.uniform(-1, 1)
+    dart_y = random.uniform(-1, 1)
+
+    myturtle.up()
+    myturtle.goto(dart_x, dart_y)
+
+    if isInCircle(myturtle):
+        myturtle.dot(size_of_dart, "lime")   
+    else:
+        myturtle.dot(size_of_dart, "darkred")
+
+def playDarts(myturtle):
+    """
+    Play a game with two players, taking turns each round
+    Print the outcome of the game to the console
+    """
+    num_of_rounds = 10
+    player1_score = 0
+    player2_score = 0
+
+    for i in range(num_of_rounds):
+        # Player 1
+        throwDart(myturtle)
+        if isInCircle(myturtle):
+            player1_score = player1_score + 1
+        # Player 2
+        throwDart(myturtle)
+        if isInCircle(myturtle):
+            player2_score = player2_score + 1
+    
+    # Print the score and the result of the game
+    print("P1:", player1_score, "| P2:", player2_score)
+
+    if player1_score > player2_score:
+        print("Player 1 won the dart game!")
+    elif player2_score > player1_score:
+        print("Player 2 won the dart game!")
+    else:
+        print("The dart game ends with a tie!")
+
+def montePi(myturtle, num_of_darts):
+    """
+    Estimate the value of PI by doing a Monte Carlo simulation
+    """
+    num_of_darts_in_circle = 0
+
+    for i in range(num_of_darts):
+        throwDart(myturtle)
+        if isInCircle(myturtle):
+            num_of_darts_in_circle = num_of_darts_in_circle + 1
+
+    pi_approximation = (num_of_darts_in_circle / num_of_darts) * 4
+    return pi_approximation
+    
 
 
 #########################################################
@@ -61,6 +164,7 @@ def main():
     darty.clear()
     setUpDartboard(window, darty)
     playDarts(darty)
+
     print("\tPart B Complete...")
     # Keep the window up until dismissed
     print("=========== Part C ===========")
